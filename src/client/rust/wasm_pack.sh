2cf55d48086679adcf576bed0cf6ca2504993491
Doug Roeper <douglas.roeper@gmail.com>
2023-08-22 08:04:12 -0400

    - Minor fixes

--- src/client/rust/wasm_pack.sh
+++ src/client/rust/wasm_pack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wasm-pack build --scope observation-tools --target nodejs --dev --features wasm
-sed -i 's/observation-tools\/observation-tools-client/observation-tools\/client/' pkg/package.json
+sed -i 's/observation-tools\/observation-tools/observation-tools\/client/' pkg/package.json
