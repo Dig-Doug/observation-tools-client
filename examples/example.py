56e95858026dad5a97e463ee0a303bab616ec5c7
Doug Roeper <douglas.roeper@gmail.com>
2024-11-27 09:01:43 -0500

    Saving

--- examples/example.py
+++ examples/example.py
@@ -1,21 +1,24 @@
-import logging
-import time
-
 from observation_tools import Client, UserMetadata, Text, Object1
 
-FORMAT = '%(levelname)s %(name)s %(asctime)-15s %(filename)s:%(lineno)d %(message)s'
-logging.basicConfig(format=FORMAT)
-logging.getLogger().setLevel(logging.DEBUG)
-
-logger = logging.getLogger(__name__)
-
-logger.info("Starting example")
+# Setup client
 client = Client("p_Tgm3GjQSZPkPWkFcjnQBAuYVNY", api_host="http://localhost:8000")
 
-logger.info("Creating run")
 run_uploader = client.create_run(UserMetadata("py_example"))
 
 run_uploader.create_object1(UserMetadata("object1"), Object1(Text("Hello, world!")))
 
-logger.info("Shutting down client")
+run_uploader.create_object1(name="llm_output", data="Hello, world!")
+
+
+def process1():
+    run_uploader.create_object1(name="ocr_output", data="Hello, world!")
+    run_uploader.create_object1(name="llm_output", data="Hello, world!")
+
+
+def process2():
+    group = run_uploader.create_group(UserMetadata("process2"))
+    group.create_object1(name="ocr_output2", data="Hello, world!")
+    group.create_object1(name="llm_output2", data="Hello, world!")
+
+
 client.shutdown()
