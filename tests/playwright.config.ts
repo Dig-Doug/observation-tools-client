11b0107a7e57b896e9a1f5c0b63df92d449baf73
Doug Roeper <douglas.roeper@gmail.com>
2025-12-07 16:12:00 -0500

    Tweak CI

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from "@playwright/test";
 
+const isCI = !!process.env.CI;
+
 export default defineConfig({
-  fullyParallel: !process.env.SERVER_URL,
   testDir: "./specs",
+  fullyParallel: !process.env.SERVER_URL,
+  forbidOnly: isCI,
+  retries: isCI ? 2 : 0,
+  workers: isCI ? 1 : undefined,
+  timeout: 60000,
+  reporter: isCI
+    ? [["github"], ["html", { outputFolder: "playwright-report", open: "never" }]]
+    : [["list"], ["html", { outputFolder: "playwright-report", open: "on-failure" }]],
+  outputDir: "test-results",
   use: {
+    trace: "on-first-retry",
+    screenshot: "only-on-failure",
     video: "retain-on-failure",
   },
-  timeout: 60000,
 });
